8e830c065770423487562cf10a0a1a8abf5a6bf2
iambillmccann <bill.mccann@gmail.com>
2020-02-07 16:56:36 -0500

    Completed the solution to the first problem

--- projecteuler/src/main/scala/ProjectEuler/EulerLibrary/Problems/Solutions.scala
+++ projecteuler/src/main/scala/ProjectEuler/EulerLibrary/Problems/Solutions.scala
@@ -1,5 +1,7 @@
 package ProjectEuler.EulerLibrary.Problems
 
+import ProjectEuler.EulerLibrary.MathLibrary
+
 trait IEulerSolution {
     def Compute : String
 }
@@ -14,13 +16,50 @@ object GenericSolution extends IEulerSolution {
 }
 
 object Multiple3or5 extends IEulerSolution {
-    /**
-      * Solve problem 1, Multiples of 3 and 5
-      *
-      * @return
-      */
+
+  val RANGESTART: Long = 1
+  val RANGESTOP: Long = 1000
+  
+  /**
+  *  Determine the number of terms in an Arithmetic Progression
+  * 
+  * @param rangeStart First number in the range
+  * @param rangeStop  Last number in the range
+  * @param difference Difference between terms
+  * @return The number of terms
+  */
+  def NumberOfTerms(rangeStart: Long, rangeStop: Long, difference: Long): Long = (rangeStop - rangeStart) / difference
+  
+  
+  /**
+  * Determine the last number in an Arithmetic Progression
+  * @param numberOfTerms The number of terms
+  * @param firstTerm     The first number
+  * @param difference    The difference between terms
+  * @return              The largest value of the sequence
+  */
+  def LastTerm(numberOfTerms: Long, firstTerm: Long, difference: Long): Long = firstTerm + difference * (numberOfTerms - 1)
+  
+   /**
+   * Solve problem 1, Multiples of 3 and 5
+   *
+   * @return The solution to the problem
+   */
     def Compute: String = {
-      Thread.sleep(2333)
-      "3.1415926"
+      val numberOfTerms3: Long = NumberOfTerms(RANGESTART, RANGESTOP, 3)
+      val numberOfTerms5: Long = NumberOfTerms(RANGESTART, RANGESTOP, 5)
+      val numberOfTerms15: Long = NumberOfTerms(RANGESTART, RANGESTOP, 15)
+      (
+        MathLibrary.ArithmeticSeries(numberOfTerms3, 
+                                     3, 
+                                     LastTerm(numberOfTerms3, 3, 3)) +
+        MathLibrary.ArithmeticSeries(numberOfTerms5, 
+                                     5, 
+                                     LastTerm(numberOfTerms5, 5, 5)) -
+        MathLibrary.ArithmeticSeries(numberOfTerms15, 
+                                     15, 
+                                     LastTerm(numberOfTerms15, 15, 15))
+      ).toString
     }
+
 }

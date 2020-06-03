82796c81ff652a5b19c8ccb1c63b995e160c9b63
Bill McCann <bill.mccann@gmail.com>
2020-06-02 23:37:27 -0400

    Getting started on problem 11.

---
+++ projecteuler/src/main/scala/ProjectEuler/EulerLibrary/Problems/LargestProductGrid.scala
@@ -0,0 +1,17 @@
+package ProjectEuler.EulerLibrary.Problems
+
+import ProjectEuler.EulerLibrary.MathLibrary
+
+object LargestProductGrid extends IEulerSolution {
+
+    /**
+      * Compute the solution to Project Euler problem number three
+      *
+      * @return The largest prime factor for 60,0851,475,143
+      */
+    def Compute: String = {
+        val result: Long  = 0L
+        return result.toString
+    }
+
+}
